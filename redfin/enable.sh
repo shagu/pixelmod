bc9c621f07a942d6cf35e7a3fa97fda42d1e4ddd
shagu <ericmauser@googlemail.com>
2020-10-30 20:54:51 +0100

    patches: add patch to remove some default apps

--- redfin/enable.sh
+++ redfin/enable.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# Add pixelmod to vendor product inclusion
 cd $ANDROID_BUILD_TOP/vendor/google_devices/redfin/proprietary/
 if ! grep -q "pixelmod" device-vendor.mk; then
  echo '$(call inherit-product-if-exists, vendor/pixelmod/$(LOCAL_STEM))' >> device-vendor.mk
 fi
+cd -
+
+# Remove ugly default apps
+cd $ANDROID_BUILD_TOP/build/target/product
+patch -N -p 1 < $ANDROID_BUILD_TOP/vendor/pixelmod/redfin/patches/remove-apps.patch
 cd -
